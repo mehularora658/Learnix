d7f7e7c3bee7f38d88c0a9d9401e2bb4d3981d9f
Mehul Arora <mehularora658@gmail.com>
2025-06-30 19:38:55 +0530

    All Courses layout Complete

--- app/workspace/_components/CourseList.jsx
+++ app/workspace/_components/CourseList.jsx
@@ -1,12 +1,26 @@
 "use client"
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AddNewCourseDialog from './AddNewCourseDialog';
+import axios from 'axios';
+import { useUser } from '@clerk/nextjs';
+import CourseCard from './CourseCard';
 
 function CourseList() {
 
     const [courseList, setCourseList] = useState([]);
+    const user = useUser();
+    useEffect(() => {
+        user && GetCourseList();
+    }, [user])
+
+
+    const GetCourseList = async () => {
+        const result = await axios.get('/api/courses');
+        console.log(result.data);
+        setCourseList(result.data)
+    }
 
     return (
         <div className='mt-10'>
@@ -19,8 +33,12 @@ function CourseList() {
                     <Button>+ Add your first course</Button>
                 </AddNewCourseDialog>
             </div> :
-                <div>
-                    List of Courses
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-5'>
+                    {
+                        courseList?.map((course, index) => (
+                            <CourseCard course={course} key={index} />
+                        ))
+                    }
                 </div>
             }
         </div>
